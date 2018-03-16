72993888940bc05365e0dc99ae69721e42ce18ba
dhyaneshbabu <dhyaneshbabu@gmail.com>
2018-03-16 07:57:46 +0530

    run analysis

---
+++ Cleaning_Tidying_Project_Excersize/run_analysis.R
@@ -0,0 +1,7 @@
+##
+#Getting and Cleaning Data Course Project
+#The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. 
+#The goal is to prepare tidy data that can be used for later analysis. 
+# The data is downloaded from the UCI Website http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+##
+
