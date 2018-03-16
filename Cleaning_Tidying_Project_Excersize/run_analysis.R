4b1820ee70cfa2915e1c296b067fd51f7bd2f934
dhyaneshbabu <dhyaneshbabu@gmail.com>
2018-03-16 08:10:27 +0530

    run analysis 1

--- Cleaning_Tidying_Project_Excersize/run_analysis.R
+++ Cleaning_Tidying_Project_Excersize/run_analysis.R
@@ -5,3 +5,21 @@
 # The data is downloaded from the UCI Website http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 ##
 
+##download the file from coursera website
+
+library(reshape2)
+
+filename <- "getdata_dataset.zip"
+
+## Download and unzip the dataset:
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+}  
+
+##unzip the file
+
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
